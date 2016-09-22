b4baec7912090bf74b23d4effa4e65e3f1bd9461
sbt-lezhnin-sa <s.lezhnin@gmail.com>
2016-09-22 18:28:56 +0500

    Hands On Clock from "http://www.lihaoyi.com/hands-on-scala-js/#MakingaClockusingsetInterval"

--- src/main/scala/example/ScalaJSExample.scala
+++ src/main/scala/example/ScalaJSExample.scala
@@ -3,9 +3,8 @@ package example
 import org.scalajs.dom
 import org.scalajs.dom.html
 
-import scala.scalajs.js.JSApp
 import scala.scalajs.js.timers.RawTimers
-import scala.util.Random
+import scala.scalajs.js.{Date, JSApp}
 
 case class Point(x: Int, y: Int) {
   def +(p: Point) = Point(x + p.x, y + p.y)
@@ -17,36 +16,37 @@ object ScalaJSExample extends JSApp {
   def main(): Unit = {
     val canvas = dom.document.getElementById("canvas")
       .asInstanceOf[html.Canvas]
-    val ctx = canvas.getContext("2d")
+    val renderer = canvas.getContext("2d")
       .asInstanceOf[dom.CanvasRenderingContext2D]
 
-    val W = ctx.canvas.width
-    val W2 = W / 2 + 1
-    val H = ctx.canvas.height
-    var count = 0
-    var p = Point(0, 0)
-    val corners = Seq(Point(W, H), Point(0, H), Point(W2, 0))
+    canvas.width = dom.window.innerWidth
+    canvas.height = dom.window.innerHeight
 
-    def clear() = {
-      ctx.fillStyle = "black"
-      ctx.fillRect(0, 0, W, H)
+    val gradient = renderer.createLinearGradient(
+      canvas.width / 2 - 100, 0, canvas.width / 2 + 100, 0
+    )
+    gradient.addColorStop(0, "red")
+    gradient.addColorStop(0.5, "green")
+    gradient.addColorStop(1, "blue")
+    renderer.fillStyle = gradient
+    //renderer.fillStyle = "black"
+
+    renderer.textAlign = "center"
+    renderer.textBaseline = "middle"
+
+    def render() = {
+      val date = new Date()
+      renderer.clearRect(
+        0, 0, canvas.width, canvas.height
+      )
+      renderer.font = "75px sans-serif"
+      renderer.fillText(
+        date.toLocaleTimeString(),
+        canvas.width / 2,
+        canvas.height / 2
+      )
     }
-
-    def run() = for (i <- 0 until 10) {
-      if (count % 3000 == 0) clear()
-      count += 1
-      p = (p + corners(Random.nextInt(3))) / 2
-
-      val height = (H * 2).toFloat / (H + p.y)
-      val r = (p.x * height).toInt
-      val g = ((H - p.x) * height).toInt
-      val b = p.y
-      ctx.fillStyle = s"rgb($g, $r, $b)"
-
-      ctx.fillRect(p.x, p.y, 2, 2)
-    }
-
-    //    dom.setInterval(() => run(), 50)
-    RawTimers.setInterval(() => run(), 50)
+    //    dom.setInterval(render _, 1000)
+    RawTimers.setInterval(render _, 1000)
   }
 }
